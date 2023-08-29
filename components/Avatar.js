34d9a594f95c9c82ca1c776c8afde238383f6c08
Pyae-Sone-Phone-Myint <codewithme201@gmail.com>
2023-08-29 21:25:30 +0630

    home page updated

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,18 @@
+// next image
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div>
+      <Image
+        src={"/avatar.png"}
+        width={737}
+        height={687}
+        alt=""
+        className=" translate-z-0 w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
